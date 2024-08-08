f6cfebf024dba6660e2d006fe51cd41cd584780c
JardeFelicio <jardefelicio@gmail.com>
2024-08-08 10:59:04 -0300

    feat: first commit

---
+++ src/main/java/com/jardefelicio/api_orders/ApiOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.jardefelicio.api_orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOrdersApplication.class, args);
+	}
+
+}
